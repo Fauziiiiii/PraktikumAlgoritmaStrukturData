814822255d9f583406df4688b7b94a045fba79b9
Fauziiiiii <fauzigoendoet99@gmail.com>
2026-02-10 11:23:37 +0700

    fix: persentase nilai uas

--- jobsheet1/PraktikumPemilihan.java
+++ jobsheet1/PraktikumPemilihan.java
@@ -6,7 +6,7 @@ public class PraktikumPemilihan {
     public static double HitungNilai(double tugas, double kuis, double uts, double uas){
         double nilaiAkhir;
 
-        nilaiAkhir = (tugas * 0.2) + (kuis * 0.2) + (uts * 0.3) + (uas * 0.4);
+        nilaiAkhir = (tugas * 0.2) + (kuis * 0.2) + (uts * 0.3) + (uas * 0.3);
 
         return nilaiAkhir;
     }
