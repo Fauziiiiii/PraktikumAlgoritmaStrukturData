9b00b1e28ec2ae79a3336219aa6146b860b819c1
Fauziiiiii <fauzigoendoet99@gmail.com>
2026-02-24 09:15:07 +0700

    Percobaan 2 - Pertanyaan 1

--- jobsheet3/MahasiswaDemo21.java
+++ jobsheet3/MahasiswaDemo21.java
@@ -24,13 +24,8 @@ public class MahasiswaDemo21 {
             System.out.println("------------------------------");
         }
 
-        for (int i = 0; i < arrayOfMahasiswa.length; i++) {
-            System.out.println("Data Mahasiswa ke-" + (i + 1));
-            System.out.println("NIM     : " + arrayOfMahasiswa[i].nim);
-            System.out.println("Nama    : " + arrayOfMahasiswa[i].nama);
-            System.out.println("Kelas   : " + arrayOfMahasiswa[i].kelas);
-            System.out.println("IPK     : " + arrayOfMahasiswa[i].ipk);
-            System.out.println("------------------------------");
+        for(Mahasiswa21 mahasiswa : arrayOfMahasiswa){
+            mahasiswa.cetakInfo();
         }
 
         sc.close();
